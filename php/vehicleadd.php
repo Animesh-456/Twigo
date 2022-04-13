5d237a9f42b8dbd4bff99447096b2859cbd90add
Animesh mondal <anim29006@gmail.com>
2022-04-13 22:16:25 +0530

    Made vehicleadd page

--- php/vehicleadd.php
+++ php/vehicleadd.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
 if ($_SESSION["remail"]) {
-	include '../php/db.php';
+  include '../php/db.php';
 
-	$remail = $_SESSION["remail"];
+  $remail = $_SESSION["remail"];
 
-	$conn = new mysqli($servername, $username, $password, $dbname);
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-	$sql = "SELECT * FROM renter WHERE R_email='$remail'";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	if ($row) {
+  $sql = "SELECT * FROM renter WHERE R_email='$remail'";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  if ($row) {
 ?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html>
 
     <head>
@@ -48,7 +48,13 @@ if ($_SESSION["remail"]) {
         input[type=number],
         input[type=tel],
         input[type=date],
-        select[id=gender] {
+        select[id=gender],
+        select[id=vtype],
+        select[id=ftype],
+        select[id=sedancname],
+        select[id=suvcname],
+        select[id=hatchbackcname],
+        select[id=evcname]{
           width: 50%;
           padding: 15px;
           margin: 5px 0px 33px 0px;
@@ -58,13 +64,21 @@ if ($_SESSION["remail"]) {
           border-radius: 30px;
         }
 
+
+
         input[type=text]:focus,
         input[type=password]:focus,
         select[id=city]:focus,
         input[type=number]:focus,
         input[type=date]:focus,
         input[type=tel]:focus,
-        select[id=gender]:focus {
+        select[id=gender]:focus,
+        select[id=vtype]:focus,
+        select[id=ftype]:focus,
+        select[id=sedancname]:focus,
+        select[id=suvcname]:focus,
+        select[id=hatchbackcname]:focus,
+        select[id=ev]:focus{
           background-color: #ddd;
           outline: none;
         }
@@ -187,51 +201,165 @@ if ($_SESSION["remail"]) {
           font-size: 22px;
           color: black;
         }
+
+        /* .container .sedan {
+          display: none;
+        }
+
+        .container .suv {
+          display: none;
+        }
+
+        .container .hatchback {
+          display: none;
+        } */
       </style>
     </head>
 
     <body>
       <form action="customersubmit.php" method="post">
         <div class="container">
-          
-            
-              
-                <br>
-                <h1>Twi<span style="color: #ee0000;">Go</span> Add Vehicle </h1>
-                <hr>
-                <br>
 
 
-                <!-- <table style="width: 100%;"> -->
-                <label for="sdate"><b>Start Date</b></label>
-                <input type="date" placeholder="" name="sdate" id="sdate" required>
 
-                <label for="edate"><b>End Date</b></label>
-                <input type="date" placeholder="" name="edate" id="edate" required>
+          <br>
+          <h1>Twi<span style="color: #ee0000;">Go</span> Add Vehicle </h1>
+          <hr>
+          <br>
 
-                <label for="days"><b>No of days</b></label>
-                <input type="number" placeholder="for how many days" name="days" id="days" required>
 
-                <label for="days"><b>Bill Amount</b></label>
-                <input type="number" placeholder="total money to pay" name="amt" id="amt" required>
+          <!-- <table style="width: 100%;"> -->
 
-                <label for="days"><b>A/C Number- 6734565784654</b></label><br>
-                <br>
-                <input type="submit" name="Book" value="Book" style="font-size: 20px;">
-                </input>
 
-                <!-- <a href="index.html" class="backbtn" style="font-size: 20px; margin-left: 250px;">Back</a> -->
-                <button type="reset" class="clearbtn" style="font-size: 20px; margin-left: 150px">Clear</button>
-              
-              
-              <!-- <td><img src="../img/undraw_travel_booking_re_6umu.svg" class="image"></img></td> -->
-            
-          
+
+
+          <label for="F_type" name="ftype"><b>Fuel Type</b></label>
+          <select id="ftype" name="ftype">
+            <option value="diesel">Diesel</option>
+            <option value="electric">Electric</option>
+          </select>
+
+          <label for="V_type" name="vtype"><b>Vehicle Type</b></label>
+          <select id="vtype" name="vtype" >
+            <option value="sedan" selected="selected">Sedan</option>
+            <option value="suv">Suv</option>
+            <option value="hatchback">Hatchback</option>
+            <option value="evsedan">EV-Sedan</option>
+          </select>
+
+          <label for="sedancname" name="sedancname"><b>Car name</b></label>
+          <div id="sedan" >
+            <select id="sedancname" name="carname">
+              <option value="Suzukidzire">Suzuki Dzire</option>
+              <option value="TataTigor">Tata Tigor</option>
+              <option value="HondaCity">Honda City</option>
+              <option value="SuzukiCiaz">Suzuki Ciaz</option>
+              <option value="HyundaiVerna">Hyundai Verna</option>
+              <option value="VolkswagenVento">Volkswagen Vento</option>
+              <option value="SkodaSlavia">Skoda Slavia</option>
+              <option value="HyundaiAura">Hyundai Aura</option>
+            </select>
+          </div>
+          <div id="suv">
+
+            <select id="suvcname"  name="carname">
+              <option value="KiaSeltos">Kia Seltos</option>
+              <option value="HyundaiCreta">Hyundai Creta</option>
+              <option value="MahindraThar">Mahindra Thar</option>
+              <option value="MahindraScorpio">Mahindra Scorpio</option>
+              <option value="TataSafari">Tata Safari</option>
+              <option value="HyundaiAlcazar">Hyundai Alcazar</option>
+              <option value="SuzukiErtiga">Suzuki Ertiga</option>
+              <option value="MahindraMarazzo">Mahindra Marazzo</option>
+              <option value="InnovaCrysta">Innova Crysta</option>
+              <option value="SuzukiXL6">Suzuki XL6</option>
+            </select>
+          </div>
+          <div id="hatchback">
+          <select id="hatchbackcname"  name="carname">
+          <option value="RenaultKwid">Renault Kwid</option>
+              <option value="HyundaiSantro">Hyundai Santro</option>
+              <option value="TataTiago">Tata Tiago</option>
+              <option value="HyundaiGrandi10nios">Hyundai Grand i10 nios</option>
+              <option value="SuzukiSwift">Suzuki Swift</option>
+              <option value="VolkswagenPolo">Volkswagen Polo</option>
+              <option value="Hyundaii20">Hyundai i20</option>
+              <option value="SuzukiWagonR">Suzuki WagonR </option>
+              <option value="TataAltroz">Tata Altroz</option>
+          </select>
+          </div>
+          <div id="ev">
+          <select id="evcname" name="carname">
+              <option value="TataTigorEV">Tata Tigor EV</option>
+          </select>
+          </div>
+
+          <!-- <label for="V_type"><b>Start Date</b></label>
+                <input type="text" placeholder="" name="sdate" id="sdate" required> -->
+
+          <label for="edate"><b>End Date</b></label>
+          <input type="date" placeholder="" name="edate" id="edate" required>
+
+          <label for="days"><b>No of days</b></label>
+          <input type="number" placeholder="for how many days" name="days" id="days" required>
+
+          <label for="days"><b>Bill Amount</b></label>
+          <input type="number" placeholder="total money to pay" name="amt" id="amt" required>
+
+          <label for="days"><b>A/C Number- 6734565784654</b></label><br>
+          <br>
+          <input type="submit" name="Book" value="Book" style="font-size: 20px;">
+          </input>
+
+          <!-- <a href="index.html" class="backbtn" style="font-size: 20px; margin-left: 250px;">Back</a> -->
+          <button type="reset" class="clearbtn" style="font-size: 20px; margin-left: 150px">Clear</button>
+
+
+          <!-- <td><img src="../img/undraw_travel_booking_re_6umu.svg" class="image"></img></td> -->
+
+
         </div>
       </form>
 
     </body>
-    <script src="JS/booking.js"></script>
+    <!-- <script src="JS/booking.js"></script> -->
+    <script>
+      var e = document.getElementById("vtype");
+      var strUser;
+      function show() {
+        var as = document.forms[0].vtype.value;
+        strUser = e.options[e.selectedIndex].value;
+        console.log(as, strUser);
+        if (strUser == "sedan") {
+          console.log("This is a sedan car");
+          document.getElementById("sedan").style.display = "block";
+          document.getElementById("suv").style.display = "none";
+          document.getElementById("hatchback").style.display = "none";
+          document.getElementById("ev").style.display = "none";
+        } else if (strUser == "suv") {
+          console.log("This is a suv car");
+          document.getElementById("sedan").style.display = "none";
+          document.getElementById("suv").style.display = "block";
+          document.getElementById("hatchback").style.display = "none";
+          document.getElementById("ev").style.display = "none";
+        } else if (strUser == "hatchback") {
+          console.log("This is a hatchback car");
+          document.getElementById("sedan").style.display = "none";
+          document.getElementById("suv").style.display = "none";
+          document.getElementById("hatchback").style.display = "block";
+          document.getElementById("ev").style.display = "none";
+        }else if(strUser=="evsedan"){
+          console.log("This is a ev car");
+          document.getElementById("sedan").style.display = "none";
+          document.getElementById("suv").style.display = "none";
+          document.getElementById("hatchback").style.display = "none";
+          document.getElementById("ev").style.display = "block";
+        }
+
+      }
+      e.onchange = show;
+      show();
+    </script>
 
     </html>
 <?php }
